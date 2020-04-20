bc6bbe238ba71c2dc7962f807eaa62f06ee3061c
Leonardo <leonardo@zungasoft.net>
2020-04-20 12:41:36 -0300

    upload de imagens

--- src/routes/users_routes.ts
+++ src/routes/users_routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticate from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // Criar usuario
 usersRouter.post('/', async (request, response) => {
@@ -24,5 +29,16 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+// patch utilizado para atalizar uma única informaçao
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticate,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
